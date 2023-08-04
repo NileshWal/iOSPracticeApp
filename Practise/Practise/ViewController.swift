3207cc298ddae028c295b215b14d2d50544ed00f
NileshWal <nileshswbvl@gmail.com>
2023-08-04 21:31:33 +0530

    Initial setup commit

---
+++ Practise/Practise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practise
+//
+//  Created by nilesh sw on 18/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
